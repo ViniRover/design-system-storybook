c8445b0cd827de62793735287e00580bd04f981d
ViniRover <viniciusrover3@gmail.com>
2023-01-29 00:50:09 -0300

    Design system with storybook

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,10 @@
+export * from './colors'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+
+export * from './radii'
+
+export * from './space'
+export * from './line-heights'
